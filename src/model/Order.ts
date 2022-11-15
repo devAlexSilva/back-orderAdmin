441d993e9d3aded07f9d71105dc499239f57c89d
Alex Silva <9apsilva@gmail.com>
2022-11-15 15:50:18 -0300

    feat: add schemas to Orders

---
+++ src/model/Order.ts
@@ -0,0 +1,38 @@
+import { model, Schema } from 'mongoose'
+
+export const Order = model(
+  'Order',
+  new Schema({
+    table: {
+      required: true,
+      type: String,
+    },
+    status: {
+      required: true,
+      type: String,
+      enum: ['WAITING', 'IN_PRODUCTION', 'DONE'],
+      default: 'WAITING',
+    },
+    createdAt: {
+      required: true,
+      type: Date,
+      default: Date.now,
+    },
+    products: {
+      required: true,
+      type: [
+        {
+          product: {
+            required: true,
+            type: Schema.Types.ObjectId,
+            ref: 'Product',
+          },
+          quantity: {
+            type: Number,
+            default: 1
+          }
+        },
+      ],
+    },
+  })
+)
