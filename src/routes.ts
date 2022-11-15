8c00748a89fa67cdd5f09d0f2b72c8cd5ac839c8
Alex Silva <9apsilva@gmail.com>
2022-11-15 18:48:16 -0300

    build: change images dir and add model to create products in multform

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,28 @@
-import { Router } from "express";
-import { CreateCategories } from "./useCases/categories/createCategories";
-import { ListCategories } from "./useCases/categories/listCategories";
+import path from 'node:path'
+
+import { Router } from 'express'
+import multer from 'multer'
+
+import { CreateCategories } from './useCases/categories/createCategories'
+import { ListCategories } from './useCases/categories/listCategories'
+import { CreateProducts } from './useCases/products/createProducts'
+import { ListProducts } from './useCases/products/listProducts'
 
 export const router = Router()
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, path.resolve(__dirname, '..', 'uploads'))
+    },
+  }),
+})
 
 router.get('/categories', ListCategories)
 router.post('/categories', CreateCategories)
 
-router.get('/products', (req, res) => res.send('ok'))
-router.post('/products', (req, res) => res.send('ok'))
+router.get('/products', ListProducts)
+router.post('/products', upload.single('image'), CreateProducts)
 
 router.get('/categories/:categoryId/products', (req, res) => res.send('ok'))
 
