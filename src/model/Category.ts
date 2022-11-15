90f8f7f8c5369b95b311ff6a75de9801e944c9dc
Alex Silva <9apsilva@gmail.com>
2022-11-15 15:37:59 -0300

    feat: add shcema catefory

---
+++ src/model/Category.ts
@@ -0,0 +1,13 @@
+import { model, Schema } from 'mongoose'
+
+export const Category = model('Category', new Schema({
+    name: {
+      required: true,
+      type: String,
+    },
+    icon: {
+      required: true,
+      type: String,
+    },
+  })
+)
