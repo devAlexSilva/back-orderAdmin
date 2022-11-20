d796229cff9c442b2a8530bf75a50255f864478d
Alex Silva <9apsilva@gmail.com>
2022-11-20 15:46:44 -0300

    feat: order by date old to new

--- src/useCases/orders/listOrders.ts
+++ src/useCases/orders/listOrders.ts
@@ -3,5 +3,8 @@ import { Order } from "../../model/Order"
 
 export async function ListOrders(req: Request, res: Response) {
     const orders = await Order.find()
+    .sort({ createdAt: 1 }) //-1 to order desc, new > old 
+    .populate('products.product')
+    
     res.json(orders)
 }
